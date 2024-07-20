5513dcca4358d7348e33ef10f7d284d6ca277487
--Leslie Mendoza <mendoxa1239@gmail.com>
2024-07-19 23:42:05 -0600

    Primer_Intento
    Intento de conectar el login con la BD y el menu principal(FALLIDO)

--- ConexionBD.py
+++ ConexionBD.py
@@ -1,14 +1,19 @@
 import pymysql
 
-
 class ConexionBD:
-    try:
-        conexion=pymysql.connect(
-            host='localhost',
-            user='root',
-            password='Mendoza1239',
-            db='Proyecto_Teleton3'
-        )
-        print("Conectado")
-    except(pymysql.err.OperationalError,pymysql.err.InternalError)as e:
-        print("Error", e)
+    def __init__(self):
+        try:
+            self.conexion = pymysql.connect(
+                host='localhost',
+                user='root',
+                password='Mendoza1239',
+                db='Proyecto_Teleton3'
+            )
+            self.cursor = self.conexion.cursor()
+            print("Conectado a la base de datos")
+        except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
+            print("Error de conexión a la base de datos:", e)
+    
+    def cerrar_conexion(self):
+        self.cursor.close()
+        self.conexion.close()
