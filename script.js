e5d2e566fa6cce7d56040e0c847987878fc2ff93
--Leslie Mendoza <mendoxa1239@gmail.com>
2024-07-19 19:58:30 -0600

    Mejora de Registro
    Se agregaron campos del registro

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const registerButton = document.getElementById("register");
 const loginButton = document.getElementById("login");
 const container = document.getElementById("container");
+window.resizeTo(800, 600); // Cambia el tamaño de la ventana a 800x600 píxeles
 
 registerButton.addEventListener("click", () => {
     container.classList.add("right-panel-active");
