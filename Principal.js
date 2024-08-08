4722410df7fc58f605749d796b9c962bcb93d9d2
--Leslie Mendoza <mendoxa1239@gmail.com>
2024-08-07 22:10:06 -0600

    Limpieza y Orden de archivos

--- Principal.js
+++
@@ -1,5 +0,0 @@
-function ingresar(role) {
-    alert('Ingresar como ' + role);
-    // Aquí puedes redirigir a la página de inicio de sesión correspondiente
-    // window.location.href = role + ".html";
-}
